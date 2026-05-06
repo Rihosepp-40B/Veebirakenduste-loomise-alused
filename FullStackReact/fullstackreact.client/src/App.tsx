c0534248d7aad87e98ff6a1576303d921a6d7d3c
Opilane TTHK <opilane@tthk.ee>
2026-05-06 13:23:51 +0300

    Lisan FullStackReact

---
+++ FullStackReact/fullstackreact.client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+interface Forecast {
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
+
+function App() {
+   
+}
+
+export default App;
