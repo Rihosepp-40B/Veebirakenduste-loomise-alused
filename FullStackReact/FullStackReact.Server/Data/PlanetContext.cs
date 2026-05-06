ca2199b5aaefda62a84ce5c8349366be547b7c46
Opilane TTHK <opilane@tthk.ee>
2026-05-06 14:11:17 +0300

    DbContext

---
+++ FullStackReact/FullStackReact.Server/Data/PlanetContext.cs
@@ -0,0 +1,20 @@
+﻿using FullStackReact.Server.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackReact.Server.Data
+{
+    public class PlanetContext : DbContext
+    {
+        public PlanetContext(DbContextOptions<PlanetContext> option) : base(option)  // constructor
+        { }
+
+        public DbSet<Planets> Planets { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Planets>().ToTable("Planets")
+        }
+
+    }
+}
